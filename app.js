4c422223d8053c2fb30d9405a600f404ef4e3832
Elote <elliotfeary@gmail.com>
2020-07-03 15:11:24 +1200

    API Get now works, added mapping file

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ const consumerSecret = "4D872FB5AF6FD6DAEE33070A49BEC496";
 const OAuthToken = "39D2C7E3D4E68AC04E0E8244ED58F170";
 const OAuthSecret = "4F34EE68371F4ED7EB77B0DF51D84589";
 
-$http.defaults.headers.common.Authorization = 'OAuth oauth_consumer_key=4D872FB5AF6FD6DAEE33070A49BEC496, oauth_token=39D2C7E3D4E68AC04E0E8244ED58F170, oauth_signature_method=PLAINTEXT, oauth_signature=160FCF77971DC92A38596288DB071A8CA5&4F34EE68371F4ED7EB77B0DF51D84589';
-
-$http.get('https://api.tmsandbox.co.nz/v1/MyTradeMe/Watchlist/All.json').
-  then(function(response) {
-    $scope.greeting = response.data;
-    console.log($scope.greeting);
-  });
-
+$http({
+  method: 'GET',
+  url: 'https://api.tmsandbox.co.nz/v1/MyTradeMe/SoldItems/Last45Days.JSON',
+  // headers: {
+  //   'Authorization':
+  //     'OAuth oauth_consumer_key= "' + consumerKey + '",' +
+  //     'oauth_signature_method = "PLAINTEXT",' +
+  //     'oauth_token = "' + OAuthToken + '",' +
+  //     'oauth_signature = "' + consumerSecret + '&' + OAuthSecret + "'
+  // }
+  headers: {
+        'Authorization':
+            'OAuth oauth_consumer_key=C5FBEDC82C96DECFA5C595C608F65576,' +
+            'oauth_signature_method=PLAINTEXT,' +
+            'oauth_token=39D2C7E3D4E68AC04E0E8244ED58F170,' +
+            'oauth_signature=4D872FB5AF6FD6DAEE33070A49BEC496&4F34EE68371F4ED7EB77B0DF51D84589'
+    }
+}).then(function success(response) {
+  $scope.k = response;
+  console.log($scope.k);
+}), function error(response) {
+  console.log('error');
+};
 
 });
