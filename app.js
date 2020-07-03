599d5b440bd0759b9f7df6f8f9ac4d47ff2c8090
Elote <elliotfeary@gmail.com>
2020-07-03 16:29:26 +1200

    Fuck async functions

--- app.js
+++ app.js
@@ -5,28 +5,36 @@ const consumerSecret = "4D872FB5AF6FD6DAEE33070A49BEC496";
 const OAuthToken = "39D2C7E3D4E68AC04E0E8244ED58F170";
 const OAuthSecret = "4F34EE68371F4ED7EB77B0DF51D84589";
 
-$http({
-  method: 'GET',
-  url: 'https://api.tmsandbox.co.nz/v1/MyTradeMe/SoldItems/Last45Days.JSON',
-  // headers: {
-  //   'Authorization':
-  //     'OAuth oauth_consumer_key= "' + consumerKey + '",' +
-  //     'oauth_signature_method = "PLAINTEXT",' +
-  //     'oauth_token = "' + OAuthToken + '",' +
-  //     'oauth_signature = "' + consumerSecret + '&' + OAuthSecret + "'
-  // }
-  headers: {
-        'Authorization':
-            'OAuth oauth_consumer_key=C5FBEDC82C96DECFA5C595C608F65576,' +
-            'oauth_signature_method=PLAINTEXT,' +
-            'oauth_token=39D2C7E3D4E68AC04E0E8244ED58F170,' +
-            'oauth_signature=4D872FB5AF6FD6DAEE33070A49BEC496&4F34EE68371F4ED7EB77B0DF51D84589'
-    }
-}).then(function success(response) {
-  $scope.k = response;
-  console.log($scope.k);
-}), function error(response) {
-  console.log('error');
-};
+//public static void main(String... args) {
+$scope.bool = false;
+
+  createSalesOrders();
+//}
+function createSalesOrders() {
+  refreshSalesList();
+}
+
+function refreshSalesList() {
+  $http({
+    method: 'GET',
+    url: 'https://api.tmsandbox.co.nz/v1/MyTradeMe/SoldItems/Last45Days.JSON',
+    headers: {
+          'Authorization':
+              'OAuth oauth_consumer_key=C5FBEDC82C96DECFA5C595C608F65576,' +
+              'oauth_signature_method=PLAINTEXT,' +
+              'oauth_token=39D2C7E3D4E68AC04E0E8244ED58F170,' +
+              'oauth_signature=4D872FB5AF6FD6DAEE33070A49BEC496&4F34EE68371F4ED7EB77B0DF51D84589'
+      }
+  }).then(function success(response) {
+    $scope.tmSalesList = response.data.List;
+    debug();
+  }), function error(response) {
+    console.log('error');
+  };
+}
+
+function debug() {
+  console.log('Debug Line');
+}
 
 });
